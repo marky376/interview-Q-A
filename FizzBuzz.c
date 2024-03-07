bf3de957e2c20cc6dc927caec6216f6009e32591
marky376 <mmutuamark@gmail.com>
2024-03-07 23:16:05 +0300

    Changes

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -7,18 +7,6 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
-
-			printf("Fizz\n");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz\n");
-		}
-		else if (i % 5 == 0 && i % 3 == 0)
-			printf("FizzBuzz\n");
-		else
-			printf("%d", i);
+		
 	}
 }
