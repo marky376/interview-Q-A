96b290dc405cbe8bbe3e015b48bb11b54663e559
marky376 <mmutuamark@gmail.com>
2024-03-07 23:06:36 +0300

    Changes

---
+++ FizzBuzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+
+			printf("Fizz");
+		}
+		if else (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("FizzBuzz");
+	}
+}
